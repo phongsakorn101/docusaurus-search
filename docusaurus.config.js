bb161ec3055f8db8f6c5c8137df3c2e4c9bd714b
Dew Techspace <dew.ts@TS.LOCAL>
2022-10-29 14:59:13 +0700

    fix url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://dewy_search.github.io/test/',
+  url: 'https://phongsakorn101.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
